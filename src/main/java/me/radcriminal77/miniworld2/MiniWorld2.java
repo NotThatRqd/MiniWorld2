265f19e564b76cabc0c593ed4eef45b6f7c6e142
NotThatRqd <67846317+NotThatRqd@users.noreply.github.com>
2023-05-31 17:16:38 -0700

    start anew

---
+++ src/main/java/me/radcriminal77/miniworld2/MiniWorld2.java
@@ -0,0 +1,13 @@
+package me.radcriminal77.miniworld2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiniWorld2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        this.getLogger().info("hello world!");
+    }
+
+}
