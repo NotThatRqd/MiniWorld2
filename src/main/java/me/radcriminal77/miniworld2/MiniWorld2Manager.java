c26e38015a0300fa4f1aee3abf2fdb5510599c14
NotThatRqd <67846317+NotThatRqd@users.noreply.github.com>
2023-06-01 12:23:00 -0700

    remove unused code

--- src/main/java/me/radcriminal77/miniworld2/MiniWorld2Manager.java
+++ src/main/java/me/radcriminal77/miniworld2/MiniWorld2Manager.java
@@ -1,27 +1,15 @@
 package me.radcriminal77.miniworld2;
 
-import org.bukkit.*;
-import org.jetbrains.annotations.Contract;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.WorldType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MiniWorld2Manager {
-
-    /**
-     * Appends "mw2." to a string if it does not already start with it
-     * @param original The string to convert
-     * @return The converted string
-     */
-    @Contract(pure = true)
-    public static String convertToMiniWorldName(@NotNull String original) {
-        if (original.startsWith("mw2.")) {
-            return original;
-        }
-
-        return "mw2." + original;
-    }
 
     private static final String MINIATURE_WORLD_PREFIX = "mw2.miniature";
 
@@ -109,37 +97,6 @@ public class MiniWorld2Manager {
                 removeMiniature(world);
             }
         }
-    }
-
-    @NotNull
-    @Deprecated
-    public static World createEmptyWorldOld(@NotNull String name) {
-        // Get the final name that is going to be used
-        String worldName = convertToMiniWorldName(name);
-
-        // Make our EmptyChunkGenerator
-        Location spawnLocation = new Location(null, 0, 64, 0);
-        EmptyChunkGenerator generator = new EmptyChunkGenerator(spawnLocation);
-
-        // Make our empty world
-        World world = new WorldCreator(worldName)
-                .generateStructures(false)
-                .generator(generator)
-                .createWorld();
-        assert world != null;
-
-        // Put a 32x1x32 pad of bedrock to start out
-        for (int x = -16; x < 16; x++) {
-
-            for (int z = -16; z < 16; z++) {
-
-                world.getBlockAt(x, 63, z).setType(Material.BEDROCK);
-
-            }
-
-        }
-
-        return world;
     }
 
 }
