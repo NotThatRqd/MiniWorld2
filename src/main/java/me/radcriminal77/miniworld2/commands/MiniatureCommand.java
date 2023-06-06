4af0b4487925790057c45d894175a4a5289fdf53
NotThatRqd <67846317+NotThatRqd@users.noreply.github.com>
2023-06-05 21:14:06 -0700

    add permissions to miniature command

--- src/main/java/me/radcriminal77/miniworld2/commands/MiniatureCommand.java
+++ src/main/java/me/radcriminal77/miniworld2/commands/MiniatureCommand.java
@@ -29,7 +29,15 @@ public class MiniatureCommand implements CommandExecutor {
         return true;
     }
 
+    private static final String  CREATE_MINIATURE_PERM = "MiniWorld2.CreateMiniature";
+    private static final String REMOVE_MINIATURE_PERM = "MiniWorld2.RemoveMiniature";
+
     private void createMiniature(@NotNull String parentWorld, @NotNull CommandSender sender) {
+        if (!sender.hasPermission(CREATE_MINIATURE_PERM)) {
+            sender.sendPlainMessage("You do not have the required permission (" + CREATE_MINIATURE_PERM + ") to create a miniature.");
+            return;
+        }
+
         // Get the world we want to create a miniature of
         World parent = Bukkit.getWorld(parentWorld);
 
@@ -44,6 +52,11 @@ public class MiniatureCommand implements CommandExecutor {
     }
 
     private void removeMiniature(@NotNull String miniatureName, @NotNull CommandSender sender) {
+        if (!sender.hasPermission(REMOVE_MINIATURE_PERM)) {
+            sender.sendPlainMessage("You do not have the required permission (" + REMOVE_MINIATURE_PERM + ") to remove a miniature.");
+            return;
+        }
+
         // Get the world we want to remove
         World miniature = Bukkit.getWorld(miniatureName);
 
