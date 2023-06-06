3bda08d23bdd12b516ebe44d70fd1d19b6812166
NotThatRqd <67846317+NotThatRqd@users.noreply.github.com>
2023-06-05 21:03:37 -0700

    add miniature command

---
+++ src/main/java/me/radcriminal77/miniworld2/commands/MiniatureCommand.java
@@ -0,0 +1,62 @@
+package me.radcriminal77.miniworld2.commands;
+
+import me.radcriminal77.miniworld2.MiniWorld2Manager;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class MiniatureCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length < 2) return false;
+
+        if (args[0].equalsIgnoreCase("create"))
+            this.createMiniature(args[1], sender);
+
+        else if (args[0].equalsIgnoreCase("remove"))
+            // remove miniature
+            this.removeMiniature(args[1], sender);
+
+        else {
+            sender.sendPlainMessage("You must either create or remove a miniature!");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void createMiniature(@NotNull String parentWorld, @NotNull CommandSender sender) {
+        // Get the world we want to create a miniature of
+        World parent = Bukkit.getWorld(parentWorld);
+
+        if (parent == null) {
+            sender.sendPlainMessage("could not find that world!");
+            return;
+        }
+
+        World miniature = MiniWorld2Manager.createMiniatureOf(parent);
+
+        sender.sendPlainMessage("Miniature world " + miniature.getName() + " created.");
+    }
+
+    private void removeMiniature(@NotNull String miniatureName, @NotNull CommandSender sender) {
+        // Get the world we want to remove
+        World miniature = Bukkit.getWorld(miniatureName);
+
+        if (miniature == null) {
+            sender.sendPlainMessage("could not find that world!");
+            return;
+        }
+
+        try {
+            MiniWorld2Manager.removeMiniature(miniature);
+        } catch (IllegalArgumentException ex) {
+            sender.sendPlainMessage("Error: " + ex.getMessage());
+        }
+    }
+
+}
